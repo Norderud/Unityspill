4ddb80d8ff06d3185b9caf4df57ecc6e3f559051
Glenn-Ove <glenn.ove.Kingestad>
2018-10-16 16:13:22 +0200

    mappe
    mappeendring

--- Smash/Assets/Scripts/Glenn/AttackTrigger.cs
+++ Smash/Assets/Scripts/Glenn/AttackTrigger.cs
@@ -12,11 +12,11 @@ public class AttackTrigger : MonoBehaviour {
         {
             if (right.GetComponent<BoxCollider2D>().enabled)
             {
-                c.collider.GetComponent<Rigidbody2D>().AddForce(new Vector2(500, 200));
+                c.collider.GetComponent<Rigidbody2D>().AddForce(new Vector2(5000, 200));
             }
             else
             {
-                c.collider.GetComponent<Rigidbody2D>().AddForce(new Vector2(-500, 200));
+                c.collider.GetComponent<Rigidbody2D>().AddForce(new Vector2(-5000, 200));
             }
         }
     }
