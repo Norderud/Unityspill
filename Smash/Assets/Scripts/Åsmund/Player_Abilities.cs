84082ce29e8fc0279db748f1d083fd5c61f30fc5
Åsmund Norderud <asmundno@live.no>
2018-10-19 18:40:45 +0200

    Fikset animations + litt mechanics

---
+++ Smash/Assets/Scripts/Åsmund/Player_Abilities.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Player_Abilities : MonoBehaviour
+{
+    public GameObject bullet;
+    public Player_Controller player;
+    public float speed = 3000f;
+    public float cooldown = 1f;
+
+    private float tTime = 0f;
+    private int facing = 1;
+
+    private void Update()
+    {
+        player.anim.SetBool("throw", false);
+        if (Input.GetButtonDown("Fire1"))
+        {
+            shoot();
+        }
+    }
+    public void shoot()
+    {
+        
+        if (tTime > Time.time) return;
+        if (player.sprite.flipX)
+        {
+            facing = -1;
+        }
+        else
+        {
+            facing = 1;
+        }
+        player.anim.SetBool("throw", true);
+
+        Vector2 startPos = new Vector2(player.GetComponent<Rigidbody2D>().position.x + (1.5f * facing), player.GetComponent<Rigidbody2D>().position.y);
+        GameObject b = Instantiate(bullet, startPos, Quaternion.identity) as GameObject;
+        b.GetComponent<Rigidbody2D>().AddForce(new Vector2(speed*facing, 0));
+        tTime = Time.time + cooldown;
+    }
+}
--- Smash/Assets/Scripts/Åsmund/Player_Controller.cs
+++ Smash/Assets/Scripts/Åsmund/Player_Controller.cs
@@ -13,19 +13,13 @@ public class Player_Controller : MonoBehaviour {
     public float speed = 400f;
     public Animator anim;
     public SpriteRenderer sprite;
-    public Shuriken shuriken;
 
     // Update is called once per frame
     void Update()
     {
-        anim.SetBool("throw", false);
         horizontal = Input.GetAxis("Horizontal");
         if (Input.GetButtonDown("Jump")) {
             jump = true;
-        }
-        if (Input.GetButtonDown("Fire1"))
-        { 
-            shuriken.shoot();
         }
     }
         private void FixedUpdate()
@@ -33,7 +27,7 @@ public class Player_Controller : MonoBehaviour {
         HandleMovement(horizontal, jump);
         jump = false;
         anim.SetFloat("velocity", velocity);
-        
+
     }
     void HandleMovement(float horizontal, bool jump)
     {
@@ -47,30 +41,27 @@ public class Player_Controller : MonoBehaviour {
           rb.velocity = new Vector2(0, 0); // resets the velocity before each jump
           rb.AddForce(new Vector2(rb.velocity.x, jumpForce));
           airJumped++;
-        } 
+        }
         // flips the animation to face moving direction
         if (horizontal < 0)
         {
             sprite.flipX = true;
-        } else if( horizontal > 0)
+        }
+        else if (horizontal > 0)
         {
             sprite.flipX = false;
         }
-            
-
-
-
     }
     // For detecting when on ground and not
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        grounded = false;
+        anim.SetBool("grounded", grounded);
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         grounded = true;
         airJumped = 0;
-        anim.SetBool("grounded", grounded);
-    }
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        grounded = false;
         anim.SetBool("grounded", grounded);
     }
 }
