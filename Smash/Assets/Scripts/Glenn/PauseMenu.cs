67cadc5622343e619e1c2bbdddad237e179e3678
Glenn-Ove <glenn.ove.kingestad@gmail.com>
2018-10-19 13:55:13 +0200

    pause

---
+++ Smash/Assets/Scripts/Glenn/PauseMenu.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    // Use this for initialization
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUi;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+	}
+
+
+    public void Resume()
+    {
+        pauseMenuUi.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    void Pause()
+    {
+        pauseMenuUi.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused =  true;
+    }
+
+    public void LoadSettings()
+    {
+        Debug.Log("Settings");
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("CharacterSelect");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
